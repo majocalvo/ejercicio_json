864879deb1bdbe21241750dc44d8314a91a935ff
Maria Jose <mjcalvoalfaro@gmail.com>
2022-09-27 01:27:40 -0400

    Desafio

---
+++ main.js
@@ -0,0 +1,128 @@
+const json = `
+[
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    }
+  ]
+  `;
+
+  //console.log(json)
+
+  const usuarios = JSON.parse(json);
+  //console.log(usuarios);
+
+  let user = ''
+
+  usuarios.forEach((e) => {
+    //user = user + `<li> ${e.id} ${e.name} ${e.username}</li>`
+    user = user + `<div id=contenedor_cards><h3>ID: ${e.id}<h3>
+    <p> Nombre: ${e.name} </p>
+    <p> Username: ${e.username}</p>
+    <p> E-mail: ${e.email}</p>
+    <p> Dirección: ${e.address.street}, ${e.address.suite}, ${e.address.city}</p>
+    <p> Código postal: ${e.address.zipcode} </p>
+    <p> Geolocalización: (${e.address.geo.lat}, ${e.address.geo.lng}) </p>
+    <p> Teléfono: ${e.phone} </p>
+    <p> Página web: ${e.website} </p>
+    <p> Compañía: ${e.company.name} </p>
+    <p> Eslogan: ${e.company.catchPhrase} </p>
+    <p> Business: ${e.company.bs} </p>
+    </div>
+
+    
+    `
+    
+  })
+
+  document.getElementById('lista').innerHTML = user
+  //console.log(user)
+
